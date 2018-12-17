f58fd9f8a321dbddfc69038e3461323bdf33e3a5
iquanzhan <iquanzhan@163.com>
2018-12-17 23:22:03 +0800

    基础表单完成

--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -1,33 +1,155 @@
 import React, { Component } from 'react';
-import { Card, Table } from 'antd';
+import { Card, Table, Modal, message, Button } from 'antd';
 
 import "../ui/ui.less";
+import axios from '../../axios';
+import Util from '../../utils/utils';
 
 class basicTable extends Component {
+
+
+    state = {
+        dataSource: [],
+        dataSource1: [],
+        dataSource2: [],
+        selectedRowKeys: '',
+        selectItem: {},
+        selectRows: [],
+        pagination: []
+    }
+
+    params = {
+        page: 1
+    }
+
+    componentDidMount() {
+        const dataSource = [
+            {
+                id: '1',
+                userName: 'Java Developer',
+                sex: 1,
+                state: 1,
+                interest: 1,
+                birthday: '2018-10-10',
+                address: '北京市海淀区知春路',
+                time: '10:10:10'
+            },
+            {
+                id: '2',
+                userName: 'React Developer',
+                sex: 0,
+                state: 2,
+                interest: 3,
+                birthday: '2018-10-10',
+                address: '北京市海淀区知春路',
+                time: '10:10:10'
+            },
+            {
+                id: '3',
+                userName: '做全栈攻城狮',
+                sex: 1,
+                state: 1,
+                interest: 1,
+                birthday: '2018-10-10',
+                address: '北京市海淀区知春路',
+                time: '10:10:10'
+            }
+        ];
+        dataSource.map((item, index) =>
+            item.key = index
+        )
+
+        this.setState({
+            dataSource
+        })
+
+        this.request();
+    }
+
+    request = () => {
+        let _this = this;
+        axios.ajax({
+            url: '/table/list',
+            data: {
+                params: {
+                    page: _this.params.page
+                },
+                isShowLoading: true
+            }
+        }).then((res) => {
+            res.result.map((item, index) =>
+                item.key = index
+            );
+            this.setState({
+                dataSource1: res.result,
+                pagination: Util.pagination(res, (current) => {
+                    debugger;
+                    _this.params.page = current;
+                    _this.request();
+                }),
+                selectedRowKeys: [],
+                selectRows: []
+            });
+        })
+
+    }
+
+
+    onRowClick = (record, index) => {
+        let selectedKey = [index];
+        Modal.info({
+            title: '信息',
+            content: `用户名：${record.userName},id:${record.id}`
+        })
+        this.setState({
+            selectedRowKeys: selectedKey,
+            selectItem: record
+        })
+    }
+
+    handleDelete = () => {
+        let rows = this.state.selectRows;
+        let ids = [];
+        rows.map((item) => {
+            ids.push(item.id)
+        })
+
+        Modal.confirm({
+            title: '删除提示',
+            content: `您确定删除这些数据吗？${ids.join(',')}`,
+            onOk: () => {
+                message.success('删除成功');
+            }
+        })
+
+    }
+
+
+
     render() {
         const column = [
             {
                 title: 'id',
                 key: 'id',
-                dataIndex="id"
+                dataIndex: "id"
             },
             {
                 title: '用户名',
                 key: 'userName',
-                dataIndex="userName"
+                dataIndex: "userName"
             },
             {
                 title: '性别',
                 key: 'sex',
-                dataIndex="sex",
+                dataIndex: "sex",
                 render(sex) {
-                    return sex == 1 ? '男' : '女';
+                    return sex === 1 ? '男' : '女';
                 }
             },
             {
-                title: 'state',
+                title: '状态',
                 key: 'state',
-                dataIndex="state",
+                dataIndex: "state",
                 render(state) {
                     let config = {
                         '1': '咸鱼一条',
@@ -41,7 +163,7 @@ class basicTable extends Component {
             {
                 title: '爱好',
                 key: 'interest',
-                dataIndex="interest",
+                dataIndex: "interest",
                 render(abc) {
                     let config = {
                         '1': '游泳',
@@ -73,15 +195,81 @@ class basicTable extends Component {
             }
 
         ];
+
+        let selectedRowKeys = this.state.selectedRowKeys;
+        const rowSelection = {
+            type: 'radio',
+            selectedRowKeys
+        };
+
+        const rowCheckSelection = {
+            type: 'checkbox',
+            selectedRowKeys,
+            onChange: (selectedRowKeys, selectRows) => {
+                this.setState({
+                    selectedRowKeys,
+                    selectRows
+                })
+            }
+        };
+
         return (
             <div>
                 <Card title="基础表格" className="card-warp">
                     <Table
                         bordered
                         columns={column}
+                        dataSource={this.state.dataSource}
                         pagination={false}
                     />
                 </Card>
+                <Card title="动态数据渲染表格">
+                    <Table
+                        bordered
+                        columns={column}
+                        dataSource={this.state.dataSource1}
+                    />
+                </Card>
+
+                <Card title="Mock-单选" className="card-warp">
+                    <Table
+                        columns={column}
+                        bordered
+                        dataSource={this.state.dataSource1}
+                        pagination={false}
+                        rowSelection={rowSelection}
+                        onRow={(record, index) => {
+                            return {
+                                onClick: () => {
+                                    this.onRowClick(record, index);
+                                }
+                            }
+                        }}
+                    />
+                </Card>
+                <Card title="Mock-多选" className="card-warp">
+                    <div>
+                        <Button type='primary' onClick={this.handleDelete} style={{ marginBottom: 10 }}>删除</Button>
+                    </div>
+                    <Table
+                        columns={column}
+                        bordered
+                        dataSource={this.state.dataSource1}
+                        pagination={false}
+                        rowSelection={rowCheckSelection}
+                    />
+                </Card>
+
+                <Card title="Mock-分页" className="card-warp">
+                    <Table
+                        columns={column}
+                        bordered
+                        dataSource={this.state.dataSource1}
+                        pagination={this.state.pagination}
+
+                    />
+                </Card>
+
             </div>
         );
     }
--- src/router.js
+++ src/router.js
@@ -14,6 +14,7 @@ import Gallery from "./pages/ui/gallery"
 import Carousel from "./pages/ui/carousels";
 import FormLogin from "./pages/form/login";
 import FormRegister from "./pages/form/register";
+import BasicTable from './pages/table/basicTable';
 
 
 class Router extends Component {
@@ -35,7 +36,7 @@ class Router extends Component {
                                 <Route path="/admin/ui/carousel" component={Carousel}></Route>
                                 <Route path="/admin/form/login" component={FormLogin}></Route>
                                 <Route path="/admin/form/reg" component={FormRegister}></Route>
-                                <Route path="/admin /table/basic" component={FormRegister}></Route>
+                                <Route path="/admin/table/basic" component={BasicTable}></Route>
                                 <Route component={NoMatch}></Route>
                             </Switch>
                         </Admin>
